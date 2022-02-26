053003499c278de99e8bb9822dc81e94067951e9
SarthakAhluwalia01 <99408881+SarthakAhluwalia01@users.noreply.github.com>
2022-02-26 22:45:31 +0530

    Add files via upload

---
+++ Core Java 11/Basic Data Structure/Assignment1Q1.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+class ArmstrongOrNot {
+    public boolean armstrongCheck(int num){
+        int a = num;
+        int arm =0;
+        while(a!=0){
+            int remainder = a%10;
+            arm =arm+ (int)Math.pow(remainder,3); // adds the cube of remainder into arm
+            a = a/10; // remove the last digit
+        }
+
+        if(num==arm)
+            return true;
+        else
+            return false;
+    }
+}
+
+
+public class Assignment1Q1 {
+
+    public static void main(String[] args) {
+
+        ArmstrongOrNot armstrongOrNot = new ArmstrongOrNot();
+        int num;
+        try (Scanner sc = new Scanner(System.in)) {
+            num = sc.nextInt();
+        }
+
+        if(armstrongOrNot.armstrongCheck(num)==true){
+            System.out.println(num + " is an armstrong number");
+        }
+        else
+        {
+            System.out.println(num + " is not an armstrong number");
+        }
+    }
+
+}
